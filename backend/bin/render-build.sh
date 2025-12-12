1bdc0d1161b615efe5938285ba84ab757b246e8a
Edward Chvainickas <chvainickas@gmail.com>
2025-12-12 16:47:12 +0000

    Add database seeding to build script

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -4,3 +4,6 @@ set -o errexit
 
 bundle install
 bundle exec rails db:migrate
+
+# Seed database if no books exist
+bundle exec rails runner "exit(Book.count > 0 ? 0 : 1)" || bundle exec rails db:seed
